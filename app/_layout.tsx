24be72e8565cc86e92eb713ede1558fbba617c43
renz0826 <brenzbedonia@gmail.com>
2026-03-23 13:28:42 +0800

    fix: set default bg to transparent

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,11 @@
+import { useTheme } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
+  const { colors } = useTheme();
+  colors.background = "transparent";
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
