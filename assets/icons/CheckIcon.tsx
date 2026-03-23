2a037841890027a0e553580ba1813fe63fc5a054
renz0826 <brenzbedonia@gmail.com>
2026-03-23 15:33:08 +0800

    refactor(icons): use Path tag instead of path

--- assets/icons/CheckIcon.tsx
+++ assets/icons/CheckIcon.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import Svg from "react-native-svg";
+import Svg, { Path } from "react-native-svg";
 
 // CheckIcon
 export default function CheckIcon({ color = "", size = 24 }) {
   return (
     <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
-      <path
+      <Path
         d="M5 13L9 17L19 7"
         stroke={color}
         stroke-width="2"
