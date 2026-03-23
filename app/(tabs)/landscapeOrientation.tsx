05910bec59d081f9e5c6d299154841905373c2f3
Fabshkiee <pwedefabros@gmail.com>
2026-03-24 06:48:26 +0800

    feat: add landscape responsiveness

--- app/(tabs)/landscapeOrientation.tsx
+++ app/(tabs)/landscapeOrientation.tsx
@@ -9,36 +9,47 @@ export default function LandscapeOrientationScreen() {
   const isLandscape = width > height;
 
   return (
-    <View className="flex-1 bg-surface-default px-8 pt-44">
+    <View
+      className={`flex-1 bg-surface-default px-8 ${
+        isLandscape ? "items-center justify-center" : "pt-44"
+      }`}
+    >
       <View className="items-center">
         {/* Robot */}
-        <View className="items-center justify-center mb-8">
+        <View className={`items-center justify-center ${isLandscape ? "mb-2" : "mb-8"}`}>
           <Image
             source={require("@/assets/mascot/MascotPhone.png")}
-            className="w-[201px] h-[229px]"
+            style={
+              isLandscape
+                ? { width: 160, height: 180 }
+                : { width: 201, height: 229 }
+            }
           />
         </View>
+      </View>
 
-        <Text className="text-lg font-text font-black text-center mb-3 leading-tight">
+      <View className="items-center">
+        <Text
+          className={`text-lg font-text font-black mb-3 leading-tight text-center`}
+        >
           For better accuracy, hold your device in landscape orientation.
         </Text>
 
         <Text
-          className={`text-md text-center font-text mb-6 ${
-            isLandscape ? "text-low" : "text-text-critical"
+          className={`text-md font-text mb-6 text-center ${
+            isLandscape ? "text-text-low" : "text-text-critical"
           }`}
         >
           {isLandscape
-            ? "Device is in landscape orientation."
+            ? "Feel free to take a photo of your room now."
             : "Device is not yet in landscape orientation."}
         </Text>
 
         <Button
           label="Take Photo"
-          className="w-full"
+          className={isLandscape ? "w-80" : "w-full"}
           disabled={!isLandscape}
           onPress={() => {
-            // Future logic: Trigger camera capture
             router.push("/");
           }}
           icon={<ArrowIcon color="white" size={20} />}
