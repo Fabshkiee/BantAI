023aa2d284230f793660d4bb1882083ae0268bbc
renz0826 <brenzbedonia@gmail.com>
2026-03-28 17:42:24 +0800

    issue: camera notification modal

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,9 +1,10 @@
+import ArrowIcon from "@/assets/icons/ArrowIcon";
 import Button from "@/components/Button";
 import { CameraView, useCameraPermissions } from "expo-camera";
 import { useFocusEffect } from "expo-router";
 import * as ScreenOrientation from "expo-screen-orientation";
 import * as React from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function CameraScreen() {
@@ -24,14 +25,14 @@ export default function CameraScreen() {
     }, []),
   );
 
-  useEffect(() => {
-    if (showNotification) {
-      const timer = setTimeout(() => {
-        setShowNotification(false);
-      }, 5000);
-      return () => clearTimeout(timer);
-    }
-  }, [showNotification]);
+  // useEffect(() => {
+  //   if (showNotification) {
+  //     const timer = setTimeout(() => {
+  //       setShowNotification(false);
+  //     }, 5000);
+  //     return () => clearTimeout(timer);
+  //   }
+  // }, [showNotification]);
 
   if (!permission) {
     return <View className="flex-1 bg-black" />;
@@ -40,13 +41,22 @@ export default function CameraScreen() {
   if (!permission.granted) {
     return (
       <View className="flex-1 justify-center items-center bg-surface-default px-8">
-        <Text className="text-center font-text font-bold text-lg mb-6 text-text-primary">
-          We need your permission to show the camera
+        <Image
+          source={require("@/assets/mascot/MascotPhone.png")}
+          style={{
+            width: 160,
+            height: 180,
+          }}
+        />
+        <Text className="text-center font-text font-semibold text-lg mt-3 mb-4 text-text-default">
+          BantAI need permission to use your camera.
         </Text>
         <Button
-          label="Grant Permission"
+          label="Allow Camera Use"
           onPress={requestPermission}
-          className="w-full"
+          className="w-56"
+          icon={<ArrowIcon color="white" size={18} />}
+          iconPosition="right"
         />
       </View>
     );
@@ -58,19 +68,19 @@ export default function CameraScreen() {
         {/* Feedback Notification Popup */}
         {showNotification && (
           <View className="absolute top-8 w-full items-center z-50">
-            <View className="bg-white rounded-[16px] pl-10 pr-6 py-3 flex-row items-center border border-gray-100 shadow-lg ml-6">
-              {/* Mascot Bubble */}
-              <View className="absolute -left-8 -top-3 w-[64px] h-[64px] rounded-full bg-[#bde0fe] items-center justify-center shadow-sm overflow-hidden">
+            <View className="relative ml-6 mr-6 mt-3">
+              <View className="bg-white rounded-[16px] pl-10 pr-6 py-3 flex-row items-center border border-gray-100 shadow-lg w-full">
+                <Text className="font-text text-md font-medium text-text-default ml-3">
+                  Perfect, you may now take the photo.
+                </Text>
+              </View>
+              <View className="absolute -left-6 -top-3 w-[64px] h-[64px] rounded-full bg-[#bde0fe] items-center justify-center shadow-sm z-10">
                 <Image
                   source={require("@/assets/mascot/MascotSmile.png")}
                   className="w-12 h-12"
                   resizeMode="contain"
                 />
               </View>
-
-              <Text className="font-text text-[15px] font-medium text-text-primary ml-2">
-                Perfect, you may now take the photo.
-              </Text>
             </View>
           </View>
         )}
