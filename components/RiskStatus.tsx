1d5cd6f74a2d5c0487e5bf3087b1be312e90a202
Renz Clyd Bedonia <renzclyd.bedonia@wvsu.edu.ph>
2026-03-31 03:11:21 +0800

    feat: create RiskStatus component

---
+++ components/RiskStatus.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+// risk level map
+type RiskLevelProps = {
+  variants: "low" | "medium" | "high" | "critical";
+};
+
+const bgStyle = {
+  low: "bg-surface-green border-2 border-border-low",
+  medium: "bg-surface-yellow border-2 border-border-medium",
+  high: "bg-surface-orange border-2 border-border-high",
+  critical: "bg-surface-red border-2 border-border-critical",
+};
+
+const textStyle = {
+  low: "text-text-low",
+  medium: "text-text-medium",
+  high: "text-text-high",
+  critical: "text-text-critical",
+};
+
+const labelText = {
+  low: "Low",
+  medium: "Medium",
+  high: "High",
+  critical: "Critical",
+};
+
+export default function RiskStatus({ variants }: RiskLevelProps) {
+  return (
+    <View
+      className={`rounded-full py-1 px-4 items-center justify-center flex self-start ${bgStyle[variants]}`}
+    >
+      <Text className={`text-md font-semibold ${textStyle[variants]}`}>
+        {" "}
+        {`${labelText[variants]}`}
+      </Text>
+    </View>
+  );
+}
