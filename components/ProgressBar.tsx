b27dc9349cec30465cbb5b8fe5c10ba7997a55bf
Renz Clyd Bedonia <renzclyd.bedonia@wvsu.edu.ph>
2026-03-30 14:19:41 +0800

    feat: create different progress states

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,13 +1,39 @@
 import Button from "@/components/Button";
+import React from "react";
 import { Text, View } from "react-native";
 import * as Progress from "react-native-progress";
 
+const brandColor = "#006ec2";
+
+// progress bar map
+type progressBarState = {
+  progressValue: number;
+  percentage: number;
+  subtext: string;
+};
+
+const progressBarStates: progressBarState[] = [
+  { progressValue: 0, percentage: 0, subtext: "Initializing..." },
+  { progressValue: 0.2, percentage: 20, subtext: "Detecting room layout..." },
+  {
+    progressValue: 0.5,
+    percentage: 50,
+    subtext: "Analyzing for disastrous hazards...",
+  },
+  {
+    progressValue: 0.8,
+    percentage: 80,
+    subtext: "Generating safety report...",
+  },
+  { progressValue: 1, percentage: 100, subtext: "Analysis Complete!" },
+];
+
 export default function ProgressBar() {
-  // const variables
-  const progressiveValue = 0;
-  const brandColor = "#006ec2";
-  const subtext = "test";
-  const percentage = 0;
+  // NOTE: Back-end Logic may be placed here.
+
+  // index default by 0
+  const indexProgress = 0;
+  const currentState = progressBarStates[indexProgress];
 
   // progress bar logic
   return (
@@ -15,12 +41,12 @@ export default function ProgressBar() {
       {/* Loading header text */}
       <View className="flex items-center">
         <Text className="text-h3 font-semibold mb-1">Analyzing Your Room</Text>
-        <Text className="mb-6"> {subtext} </Text>
+        <Text className="mb-6"> {currentState.subtext} </Text>
       </View>
 
       {/* ProgressBar properties */}
       <Progress.Bar
-        progress={progressiveValue}
+        progress={currentState.progressValue}
         width={320}
         height={12}
         borderRadius={12}
@@ -31,14 +57,14 @@ export default function ProgressBar() {
         color={brandColor}
       />
       <Text className="text-center mt-2 text-lg font-semibold color-text-primary mb-6">
-        {percentage} %
+        {currentState.percentage} %
       </Text>
 
       {/* Cancel Button */}
       <Button
         label="Cancel"
         className="w-full"
-        onPress={() => null} // This should cancel ta operation
+        onPress={() => null}
         variant="cancel"
       />
     </View>
