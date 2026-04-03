c52a4a32a7df0fc132079f4f8e7fb7a44b3bca83
renz0826 <brenzbedonia@gmail.com>
2026-04-03 09:08:35 +0800

    feat: implement sorting buttons to safetyReport screen

--- app/(tabs)/safetyReport.tsx
+++ app/(tabs)/safetyReport.tsx
@@ -1,6 +1,7 @@
 import RefreshIcon from "@/assets/icons/RefreshIcon";
 import Button from "@/components/Button";
 import HazardCard from "@/components/HazardCard";
+import HazardSortingButtons from "@/components/HazardSortingButons";
 import MascotReporter, { getRiskVariant } from "@/components/MascotReporter";
 import { useRouter } from "expo-router";
 import { ScrollView, Text, View } from "react-native";
@@ -9,9 +10,9 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 export default function SafetyReport() {
   const router = useRouter();
 
-  {
-    /* TO DO: create a function to solve the room score */
-  }
+  var executeDatabaseSearch; // TO DO: Create a function that searches the db (change to const)
+
+  // TO DO: create a function to solve the room score */
   const roomScore = 15;
   const riskVariant = getRiskVariant(roomScore);
   const insets = useSafeAreaInsets();
@@ -20,9 +21,9 @@ export default function SafetyReport() {
     <ScrollView
       className="flex-1 pt-9 pb-56 bg-surface-default"
       showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingBottom: insets.bottom + 20 }}
+      contentContainerClassName="pb-20"
     >
-      <View className="mx-7">
+      <View className="mx-7 gap-7">
         {/* Safety Report Header */}
         <View className="flex-1 justify-center items-center gap-4">
           <Text className="text-h2 font-bold text-center mt-10">
@@ -45,14 +46,22 @@ export default function SafetyReport() {
           </Text>
         </View>
 
+        <View>
+          {/* TO DO: define parameters */}
+          <HazardSortingButtons
+            tableName="test"
+            onSortQueryChange={executeDatabaseSearch}
+          />
+        </View>
+
         {/* TO DO: modify hazard card to determine risks */}
         {/* Hazard Cards */}
-        <View className="mt-7">
+        <View>
           <HazardCard />
         </View>
 
         {/* Return Buttons */}
-        <View className="w-full gap-4 mt-7">
+        <View className="w-full gap-4">
           <Button
             label="Rescan Room"
             onPress={() => {
