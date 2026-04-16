1b49afc79b29fc455842249a4c2154e139383674
Kemji <kurtandrieasoy17@gmail.com>
2026-04-16 23:22:59 +0800

    Refactor HomeScreen and MockScreen components to remove onboarding restart functionality and enhance user guidance with tutorial replay option

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,6 @@ import ScanIcon from "@/assets/icons/ScanIcon";
 import ArticleCard from "@/components/ArticleCard";
 import Button from "@/components/Button";
 import TopNavBar from "@/components/TopBar";
-import { resetOnboardingState } from "@/lib/onboardingStorage";
 import { useRouter } from "expo-router";
 import { useTranslation } from "react-i18next";
 import { Image, ScrollView, Text, View } from "react-native";
@@ -11,18 +10,13 @@ export default function HomeScreen() {
   const router = useRouter();
   const { t } = useTranslation();
 
-  const handleRestartOnboarding = async () => {
-    await resetOnboardingState();
-    router.replace("/onboarding");
-  };
-
   return (
     <ScrollView
       className="flex-1 mt-9 bg-surface-default"
       contentContainerClassName="pb-40"
       showsVerticalScrollIndicator={false}
     >
-      <View className="gap-7">
+      <View className="gap-7 mb-14">
         <TopNavBar />
 
         {/* Container to seperate nav bar and the actual content */}
@@ -46,14 +40,6 @@ export default function HomeScreen() {
               }}
               className="w-full"
             />
-            <Button
-              label="Restart Onboarding"
-              variant="secondary"
-              onPress={() => {
-                void handleRestartOnboarding();
-              }}
-              className="w-full"
-            />
           </View>
 
           {/* Articles */}
@@ -63,6 +49,21 @@ export default function HomeScreen() {
             </Text>
             <ArticleCard />
           </View>
+
+          <Text className="text-h3 font-bold mb-4 mt-6">
+              {t("Feeling Lost?")}
+            </Text>
+            <Text className="text-md mb-4 text-text-subtle mb-10">
+              {t("Don't worry, we can show you around again! Tap the button below for a quick refresher on how to spot hazards and secure your space.")}
+            </Text>
+
+          <Button
+              label="Replay App Tutorial"
+              onPress={() => {
+                router.push("/mockScreens");
+              }}
+              className="w-full"
+            />
         </View>
       </View>
     </ScrollView>
